e4da7b882cde2938f19674233c3cca77f2f49180
MaximKanevskiy <feralfrenzy430@gmail.com>
2024-01-29 16:14:03 +0300

    Added a feature that allows you to separate files for backend and frontend.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 int main() 
 {
+	std::ifstream file("www/index.html");
+	std::stringstream buffer;
+	buffer << file.rdbuf();
+	std::string page = buffer.str();
 	std::cout << "Hello, World!" << std::endl;
+	std::cout << page << std::endl;
 	return 0;
 }
